6258337cb18c430c88572c8adbcfc372931d7ed7
tanmay <tanmay.kathane@smartdings.com>
2025-05-20 23:46:34 +0530

    Resolve merge conflict and add tailwind file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="p-4">
-      <h1 className="text-4xl font-bold text-red-500"> Hello PopcornSync 🎬</h1>
+      <h1 className="text-4xl font-bold text-red-500">Hello PopcornSync 🎬</h1>
     </div>
   );
 }
