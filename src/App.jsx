0e6a706a366656de5d04e9a76b853a447872852e
Tanmay Kathane <TanmayKathane68@gmail.com>
2026-02-07 19:46:09 +0530

    Refactor App component to use state for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
-import CreateRoom from "./pages/CreateRoom";
 import Room from "./pages/Room";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/create-room" element={<CreateRoom />} />
-      <Route path="/room/:roomId" element={<Room />} />
-    </Routes>
-  );
+  const [page, setPage] = useState("home");
+
+  if (page === "room") {
+    return <Room />;
+  }
+
+  return <Home onCreate={() => setPage("room")} />;
 }
 
 export default App;
