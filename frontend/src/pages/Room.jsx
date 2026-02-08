4411be949abb1e9e460c174ab69c9b0184811abb
tanmay <tanmaykathane68@gmail.com>
2026-02-08 23:50:08 +0530

    refactor: restructure project into frontend and backend folders

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -1,8 +1,20 @@
 import VideoPlayer from "../components/VideoPlayer";
 import ChatPanel from "../components/ChatPanel";
 import RoomHeader from "../components/RoomHeader";
+import { useEffect } from "react";
+import socket from "../socket";
 
 export default function Room() {
+  const roomId = "test-room"; // abhi hardcoded
+
+  useEffect(() => {
+    socket.emit("join-room", roomId);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="h-screen bg-[#0B0B0F] flex flex-col">
       <RoomHeader />
