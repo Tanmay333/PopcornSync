b7a9467ece43c6f9126efcbd83b013f5805e905a
tanmay <tanmaykathane68@gmail.com>
2026-02-13 22:16:06 +0530

    update chatpanel adding keydown event

--- frontend/src/components/ChatPanel.jsx
+++ frontend/src/components/ChatPanel.jsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 
 export default function chatpanel() {
   const [messages, setMessages] = useState([]);
-
   const [input, setInput] = useState("");
+
+  const handleSend = () => {
+    if (!input.trim()) return;
+
+    setMessages((prev) => [...prev, input]);
+    setInput("");
+  };
 
   return (
     <div className="w-[300px] h-[500px] bg-[#111] text-white rounded-xl flex flex-col">
@@ -23,10 +29,18 @@ export default function chatpanel() {
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSend();
+            }
+          }}
           placeholder="Type a message..."
           className="flex-1 bg-[#222] px-3 py-2 rounded-lg text-sm outline-none"
         />
-        <button className="bg-[#6C63FF] px-4 py-2 rounded-lg text-sm">
+        <button
+          onClick={handleSend}
+          className="bg-[#6C63FF] px-4 py-2 rounded-lg text-sm"
+        >
           Send
         </button>
       </div>
